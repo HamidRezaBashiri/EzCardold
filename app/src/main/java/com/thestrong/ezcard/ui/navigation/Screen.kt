77ad563cb1a6d90c8ba8c0b48003611b2e2862fc
HamidReza Bashiri <HamidRezaBashiri@protonmail.com>
2022-01-13 15:05:12 +0330

    fix/layout direction bottom bar direction

--- app/src/main/java/com/thestrong/ezcard/ui/navigation/Screen.kt
+++ app/src/main/java/com/thestrong/ezcard/ui/navigation/Screen.kt
@@ -4,8 +4,8 @@ import androidx.annotation.DrawableRes
 import com.thestrong.ezcard.R
 
 sealed class Screen(val route: String, val title: String, @DrawableRes val icon: Int){
-    object Wallet:Screen("wallet","CardList",R.drawable.ic_wallet)
-    object Home :Screen("home","Home", R.drawable.ic_home)
     object Settings:Screen("setting","Setting",R.drawable.ic_setting)
+    object Home :Screen("home","Home", R.drawable.ic_home)
+    object Wallet:Screen("wallet","CardList",R.drawable.ic_wallet)
 
 }
--- app/src/main/java/com/thestrong/ezcard/ui/screens/signIn/SignInScreen.kt
+++ app/src/main/java/com/thestrong/ezcard/ui/screens/signIn/SignInScreen.kt
@@ -2,21 +2,22 @@ package com.thestrong.ezcard.ui.screens.signIn
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.thestrong.ezcard.data.model.User
-import com.thestrong.ezcard.ui.screens.signUp.SignUpViewModel
+import androidx.compose.ui.unit.dp
 import com.thestrong.ezcard.ui.theme.LoginBoxBackgroundDark
 import com.thestrong.ezcard.ui.theme.LoginBoxBackgroundLight
 import com.thestrong.ezcard.ui.theme.SplashBackgroundDark
 import com.thestrong.ezcard.ui.theme.SplashBackgroundLight
-import org.koin.androidx.compose.viewModel
 
 
 @Composable
@@ -31,7 +32,9 @@ fun SignInScreen(isFirstTimeLunch: Boolean = true) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(spBackgroundColor)
+            .background(spBackgroundColor),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         AnimatedLogo()
         if (isFirstTimeLunch) {
@@ -48,7 +51,7 @@ fun SignupBox() {
 
 @Composable
 fun LoginBox() {
-    val viewModel by viewModel<SignUpViewModel>()
+//    val viewModel by viewModel<SignUpViewModel>()
     val signInBoxBackground: androidx.compose.ui.graphics.Color
     var textFiledState by remember {
         mutableStateOf("")
@@ -59,18 +62,19 @@ fun LoginBox() {
     } else {
         signInBoxBackground = LoginBoxBackgroundLight
     }
-    Column(modifier = Modifier.background(signInBoxBackground)) {
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colors.surface)
+            .padding(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(text = "سلام خوش آمدید")
         OutlinedTextField(
             value = textFiledState,
             label = { Text(text = "کلمه عبور خود را وارد کنید") },
             onValueChange = { textFiledState = it })
-        Button(onClick = {
-            val user = User(textFiledState, textFiledState)
-            viewModel.signUp(user)
-        }) {
-
-        }
+        Button(onClick = {}, modifier = Modifier.padding(8.dp),
+            content = { Text(text = "test") })
     }
 }
 
