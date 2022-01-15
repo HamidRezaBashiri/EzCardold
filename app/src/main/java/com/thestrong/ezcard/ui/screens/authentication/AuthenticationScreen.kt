f702dadfcf1850e3574efa6f0259a7f194ecdbb0
HamidReza Bashiri <HamidRezaBashiri@protonmail.com>
2022-01-15 18:21:15 +0330

    ui/login

--- app/src/main/java/com/thestrong/ezcard/ui/screens/authentication/AuthenticationScreen.kt
+++ app/src/main/java/com/thestrong/ezcard/ui/screens/authentication/AuthenticationScreen.kt
@@ -4,20 +4,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.thestrong.ezcard.R
-import com.thestrong.ezcard.ui.theme.LoginBoxBackgroundDark
-import com.thestrong.ezcard.ui.theme.LoginBoxBackgroundLight
-import com.thestrong.ezcard.ui.theme.SplashBackgroundDark
-import com.thestrong.ezcard.ui.theme.SplashBackgroundLight
+import com.thestrong.ezcard.ui.theme.*
 import org.koin.androidx.compose.viewModel
 
 
@@ -50,12 +57,16 @@ fun SignupBox() {
 //    TODO("Not yet implemented")
 }
 
+@Preview
 @Composable
 fun LoginBox() {
     val viewModel by viewModel<AuthenticationViewModel>()
     val signInBoxBackground: androidx.compose.ui.graphics.Color
     var textFiledState by rememberSaveable {
         mutableStateOf("")
+    }
+    var passwordVisibility by remember {
+        mutableStateOf(false)
     }
 
     if (isSystemInDarkTheme()) {
@@ -65,21 +76,52 @@ fun LoginBox() {
     }
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 48.dp, vertical = 16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.Start
+            .padding(16.dp)
+            .clip(RoundedCornerShape(15))
+            .background(color = MaterialTheme.colors.surface),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "سلام")
-        Text(text = "خوش آمدید")
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 48.dp, vertical = 16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(text = "سلام", fontSize = 20.sp, color = MaterialTheme.colors.onSurface)
+            Text(text = "خوش آمدید", color = MaterialTheme.colors.onSurface)
+        }
+
+        OutlinedTextField(
+            value = textFiledState,
+            label = { Text(text = "کلمه عبور خود را وارد کنید") },
+            onValueChange = { textFiledState = it },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Purple500 ,
+                unfocusedBorderColor = MaterialTheme.colors.onSurface ,
+                placeholderColor = MaterialTheme.colors.onSurface,
+                textColor = MaterialTheme.colors.onSurface,
+                errorTrailingIconColor =MaterialTheme.colors.error,
+                focusedLabelColor = Purple500 ,
+                unfocusedLabelColor = MaterialTheme.colors.onSurface,
+            ),
+            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                val image = if (passwordVisibility)
+                    Icons.Filled.Menu
+                else Icons.Filled.ShoppingCart
+
+                IconButton(onClick = {
+                    passwordVisibility = !passwordVisibility
+                }) {
+                    Icon(imageVector = image, "")
+                }
+            })
+        Button(onClick = {}, modifier = Modifier.padding(16.dp),
+            content = { Text(text = "ورود") })
     }
 
-    OutlinedTextField(
-        value = textFiledState,
-        label = { Text(text = "کلمه عبور خود را وارد کنید") },
-        onValueChange = { textFiledState = it })
-    Button(onClick = {}, modifier = Modifier.padding(16.dp),
-        content = { Text(text = "test") })
 }
 
 
